b63b1d265f01ee9f3ad8e395c6a25c6098e6fd54
Defragster <mobiletb>
2016-03-05 16:48:33 -0800

    added enum removed all delay() reduced ADC read speed

--- Nixie_Blink/Nixie_Blink.ino
+++ Nixie_Blink/Nixie_Blink.ino
@@ -12,17 +12,26 @@ uint16_t arrVBAT[8];
 uint16_t iiVBAT = 0;
 uint8_t ledRGB[3] = { ledPin1, ledPin2, ledPin3};
 
+enum ColorND { white, purple, orange, red, teal, blue, green, black };
+void showColor( int16_t sCol )
+{  // sCol == enum ColorND
+  digitalWrite(ledRGB[ 0 ], (sCol & 1));  // Given sCol these three lines make enumerated color for function
+  digitalWrite(ledRGB[ 1 ], (sCol & 2));
+  digitalWrite(ledRGB[ 2 ], (sCol & 4));
+}
+
 void setup()
 {
   Serial.begin(9600);
   for ( int jj = 0; jj < 3; jj++ ) pinMode(ledRGB[ jj ], OUTPUT);
   pinMode(VbatPin, INPUT); //pin 15 (A1) single ended
-
+  showColor( red ); delay(500);  showColor( white );  delay(500);  showColor( blue );  delay(500);
+  showColor( red ); delay(500);  showColor( white );  delay(500);  showColor( blue );  delay(500);
   ///// ADC0 ////
   // reference can be ADC_REF_3V3, ADC_REF_1V2 (not for Teensy LC) or ADC_REF_EXT.
   adc->setReference(ADC_REF_3V3, ADC_0); // change all 3.3 to 1.2 if you change the reference to 1V2
 
-  adc->setAveraging(3); // set number of averages
+  adc->setAveraging(1); // set number of averages
   adc->setResolution(12); // set bits of resolution
 
   // it can be ADC_VERY_LOW_SPEED, ADC_LOW_SPEED, ADC_MED_SPEED, ADC_HIGH_SPEED_16BITS, ADC_HIGH_SPEED or ADC_VERY_HIGH_SPEED
@@ -32,17 +41,12 @@ void setup()
   adc->setSamplingSpeed(ADC_LOW_SPEED); // change the sampling speed
 }
 
-void showColor( int16_t sCol )
-{
-  // sCol == enum ColorND { white, purple, orange, red, teal, blue, green, black };
-  digitalWrite(ledRGB[ 0 ], (sCol & 1));  // Given sCol these three lines make enumerated color for function
-  digitalWrite(ledRGB[ 1 ], (sCol & 2));
-  digitalWrite(ledRGB[ 2 ], (sCol & 4));
-}
+#define S_TIME  200
+#define L_TIME  1000
 
 elapsedMillis busyTime;
 elapsedMillis TimeADC;
-uint32_t busyWait = 1000;
+uint32_t busyWait = L_TIME;
 int ii = 0;
 void loop()
 {
@@ -50,13 +54,12 @@ void loop()
     if ( ii >= 8 ) ii = 0;
     busyTime = 0;
     showColor(ii++);
-    if ( 8 == ii ) for ( int jj = 1; jj < 7; jj++ ) {
-        delay(100);
-        showColor(jj);
-      }
+    if ( 8 == ii ) {
+      if ( L_TIME == busyWait ) busyWait = S_TIME; else busyWait = L_TIME;
+    }
   }
 
-  if ( TimeADC > 100 ) {
+  if ( TimeADC > 120 ) {
     TimeADC = 0;
     arrVBAT[ iiVBAT ] = adc->analogRead(VbatPin); // read a new value, will return ADC_ERROR_VALUE if the comparison is false.
     if ( 0 != arrVBAT[ iiVBAT ] ) iiVBAT++;
